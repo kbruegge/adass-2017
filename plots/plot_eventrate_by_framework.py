0c136c606bc3c0fad57590c654757803a58b04ab
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2017-11-08 16:41:46 +0000

    style changes

--- plots/plot_eventrate_by_framework.py
+++ plots/plot_eventrate_by_framework.py
@@ -9,6 +9,9 @@ import seaborn as sns
 @click.argument('output_file', type=click.Path(exists=False))
 @click.option('--label', '-l', multiple=True, help='the labels of the groups')
 def main(input_files, output_file, label):
+    if not len(input_files) == len(label):
+        print('Number of input files has to be equal to number of provided labels')
+        return
 
     result = pd.DataFrame()
     print(label)
@@ -17,16 +20,14 @@ def main(input_files, output_file, label):
 
     for i, f in enumerate(input_files):
         df = pd.read_csv(f)
-        # drop first line
-        df = df[1:]
+        # drop first 5 lines as outliers
+        df = df[15:]
         datarate = df['@datarate']
         result['{}'.format(label[i])] = datarate
 
     # result = result.reindex_axis(natsorted(result.columns), axis=1)
 
-    sns.set_style('darkgrid')
-    with sns.color_palette(['#3697f1', '#ff8600', ]):
-        sns.violinplot(data=result, linewidth=1.2, fliersize=1)
+    sns.stripplot(data=result, jitter=0.02)
 
     sns.despine()
     plt.ylabel('Events per Second')
